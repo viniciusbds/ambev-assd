a0e1cc2f902b04f401f11f0668082cc7244a8c8f
vinicius.barbosa <vinicius.barbosa@virtus.ufcg.edu.br>
2021-10-21 15:36:39 -0300

    Add Supply site model

---
+++ model/SupplySite.py
@@ -0,0 +1,24 @@
+class SupplySite:
+
+    def __init__(self, code, latitude, longitude):
+        self.code = code
+        self.latitude = latitude
+        self.longitude = longitude
+        self.availableToDeploy = {}       # ex  {"SKU1": 10000,43 hl}   AVAILABLE TO DEPLOY
+        self.capacity = {}                # ex {"SKU1": (min, reorder, max)}
+        self.closingStock = {}            # ex  {"SKU1": 1000,43 hl}
+
+
+    def addSKUsToDeploy(self, sku, number):
+        self.availableToDeploy[sku] += number
+
+
+    def setSKUCapacity(self, sku, min, reorder, max):
+        self.capacity[sku] += (min,reorder,max)
+
+
+    def setSKUClosingStock(self, sku, closingStock):
+        self.closingStock[sku] += closingStock
+
+
+    
