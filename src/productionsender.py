444fdde8f2159d6140d2b092ce2191386256c4c3
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2021-10-24 23:38:43 -0300

    Finish productionsender code

--- src/productionsender.py
+++ src/productionsender.py
@@ -1,26 +1,34 @@
-import sender
 
-def sendProduction(supplySites):
-    #TODO
+distribution = {}   # key   = supplysiteCode + ":" + SKU + ":" + depotCode  
+                    # value = amount of SKU (in Hl) to send from supplysiteCode to depotCode
+
+def sendProduction(supplySites, depots):
     for key in supplySites:
         supplySite = supplySites[key]
 
-        print(supplySite)
-
         for SKU in supplySite.availableToDeploy:
-            availableToDeploy = supplySite.availableToDeploy[SKU]
-            depotsSortedByNeed = sortDepotsByNeed("", SKU)
-            # print(SKU + " - " + availableToDeploy)
+            recipients = supplySite.recipients[SKU] # CDDs que estão na lista de envios da fábrica atual
+            availableToDeploy = supplySite.availableToDeploy[SKU] 
+            depotsSortedByNeed = sortDepotsByNeed(recipients, depots, SKU)
 
-            priorityBasedDistribution(SKU, availableToDeploy, depotsSortedByNeed)
+            priorityBasedDistribution(supplySite.code, SKU, availableToDeploy, depotsSortedByNeed)
 
-        print("----------")
+    return distribution
 
-def sortDepotsByNeed(depots, sku):
-    # TODO
-    pass
 
-def priorityBasedDistribution(SKU, availableToDeploy, depotsSortedByNeed):
+# Ordena os depósitos de acordo com suas necessidades para o SKU, da maior para o menor
+def sortDepotsByNeed(recipients, depots, SKU):
+    resultList = []
+
+    for depotCode in recipients:
+        resultList.append(depots[depotCode])
+
+    resultList.sort(key=lambda depot: depot.calculeNeed(SKU), reverse=True)
+
+    return resultList
+
+
+def priorityBasedDistribution(supplysite, SKU, availableToDeploy, depotsSortedByNeed):
     while availableToDeploy > 0:
         for depot in depotsSortedByNeed:
             priority = depot.calculeNeed(SKU)
@@ -29,5 +37,13 @@ def priorityBasedDistribution(SKU, availableToDeploy, depotsSortedByNeed):
             if availableToDeploy < hectolitersToSend:
                 hectolitersToSend = availableToDeploy
             
-            sender.sendTo(depot, SKU, hectolitersToSend)
+            sendFromTo(supplysite, depot, SKU, hectolitersToSend)
             availableToDeploy -= hectolitersToSend
+        
+    
+def sendFromTo(supplysiteCode, depot, SKU, toSend):
+    key = supplysiteCode + ":" + SKU + ":" + depot.code
+    if key in distribution:
+        distribution[key] += toSend
+    else:
+        distribution[key] = toSend
