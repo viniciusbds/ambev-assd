b8957957f41798fd78903753e8940100bbf36169
viniciusbds <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2021-10-26 23:08:43 -0300

    add constants file

---
+++ constants.py
@@ -0,0 +1,11 @@
+IDEAL_STOCK_LEVEL = 1.0         # 100% of desired capacity
+CRITICAL_STOCK_LEVEL = 0.3      # 30%  of desired capacity
+SAFETY_MIN_STOCK_LEVEL =  0.6   # 60%  of desired capacity
+
+DATASET_PATH = './input/data.csv'
+LOCATIONS_PATH = './input/localization.csv'
+DISTANCE_RESULT_PATH = './input/distances.csv'
+
+RESULTS_DIR = './results'
+
+
